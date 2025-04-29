6a0b9fe3f29f9354742ea1f5a6a51ebed3f27c34
gopalpriye <govindkumaryadav8651@gmail.com>
2025-04-29 12:55:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 api_id = "18116358"
 api_hash = "80e7597f27a57df271dfc500120d4ea9"
 bot_token = "7207739619:AAF9kZhpI9YbRa4-QUtyfH7VUmKw3RtjBww"
+auth_users = [1477222048,7836088695]
+chat_id = -1002385747075  # अपना असली चैट ID यहां डालें (int में)
+log_channel_id = -1002385747075
